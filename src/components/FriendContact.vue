99e4ec4a613a6dfd58f0f59f58df6e991a538847
juliadud <yuliiadudkina@gmail.com>
2021-11-04 18:44:31 +0200

    new friend functionality

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,22 +1,72 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }} {{ isFavorite ? '(My Favorite)': ''}}</h2>
     <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleFavorite">Toggle Favorite</button>
     <ul v-if="detailsAreVisible">
       <li>
         <strong>Phone:</strong>
-        {{ friend.phone }}
+        {{ phoneNumber }}
       </li>
       <li>
         <strong>Email:</strong>
-        {{ friend.email }}
+        {{ emailAddress }}
       </li>
     </ul>
+    <button @click="deleteFriend">Delete</button>
   </li>
 </template>
 
 <script>
 export default {
+  // Camel Case for props
+  // props: [ 'name','phoneNumber','emailAddress', 'isFavorite'],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+      validator: function (email) {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      }
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  // emits: ['toggle-favorite'],
+  emits: {
+    'toggle-favorite': function (id){
+      if(id){
+        return true;
+      } else {
+        console.warn('Id is missing');
+        false;
+      }
+    },
+    'delete-friend': function (id){
+      if(id){
+        return true;
+      } else {
+        console.warn('Id is missing');
+        false;
+      }
+    }
+  },
   data() {
     return {
       detailsAreVisible: false,
@@ -31,6 +81,13 @@ export default {
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite(){
+        // this.isFriendFavorite = !this.isFriendFavorite;
+      this.$emit('toggle-favorite',this.id); // always with dash
+    },
+    deleteFriend(){
+      this.$emit('delete-friend',this.id);
     }
   }
 };
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="name">Name</label>
+      <input v-model="enteredName" type="text" name="name" id="name">
+    </div>
+    <div>
+      <label for="phone">Phone number</label>
+      <input v-model="enteredPhone" type="tel" name="phone" id="phone">
+    </div>
+    <div>
+      <label for="email">Email address</label>
+      <input v-model="enteredEmail" type="email" name="email" id="email">
+    </div>
+    <div>
+      <button>Add Contact</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewFriend",
+  emits:['add-contact'],
+  data(){
+    return{
+      enteredName: '',
+      enteredPhone: '',
+      enteredEmail: '',
+    }
+  },
+  methods:{
+    submitData(){
+      this.$emit(
+          'add-contact',
+          this.enteredName,
+          this.enteredPhone,
+          this.enteredEmail,
+      )
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
